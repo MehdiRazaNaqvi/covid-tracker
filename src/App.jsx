ded3f972de572cc8e980a210273401e628fbbc1d
MehdiRazaNaqvi <mehdiraza1232016@gmail.com>
2022-04-26 08:48:08 +0500

    search by country

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,24 @@ import { useEffect } from "react"
 const App = () => {
 
 
-    const [apidata, setapidata] = useState({ Countries: [] });
+    const [apidata, setapidata] = useState([]);
+    const [globaldata, setglobaldata] = useState({Global : {NewDeaths : Number , NewConfirmed : Number , TotalDeaths: Number , TotalConfirmed : Number , TotalRecovered : Number , Date : ""}});
 
-    // const [citysearch , setcitysearch] = useState();
+    const [citysearch , setcitysearch] = useState("");
 
-    console.log(apidata)
+
 
 
     useEffect(() => {
 
 
-        fetch(`https://api.covid19api.com/summary`).then((res) => res.json()).then((data) => setapidata(data))
+        fetch(`https://api.covid19api.com/summary`).then((res) => res.json()).then((data) => { setapidata(data.Countries); setglobaldata(data) })
 
 
     }, [])
 
+    // console.log(globaldata)
+    // console.log(apidata)
 
 
     return (
@@ -29,49 +32,109 @@ const App = () => {
         <div className="parent" >
 
 
-            <div className="uper" ></div>
 
-            <div className="nechy">
 
-                {apidata.Countries.map((v,i) => v.Country == "Pakistan" ?
+            <div className="uper" >
 
-                    <div key={i} >
+
+                <span>
+                    Total Confirmed : {globaldata.Global.TotalConfirmed}
+                </span>
+
+
+
+                <span>
+                    Total Deaths : {globaldata.Global.TotalDeaths}
+                </span>
+
+                <span>
+                    New Confirmed : {globaldata.Global.NewConfirmed}
+
+                </span>
+
+
+                <span>
+                    New Deaths : {globaldata.Global.NewDeaths}
+                </span>
+
+                <span>
+                    Total Recovered : {globaldata.Global.TotalRecovered}
+                </span>
+
+                <span>
+                    {globaldata.Global.Date}
+                </span>
+
+
+
+
+            </div>
+
+
+
+
+
+            <div className="middle">
+                <input type="text" className="form-control" onChange={(e) => setcitysearch( e.target.value )  } placeholder="Enter country" /> 
+                <button className="btn btn-outline-dark" onClick={() => {setcitysearch({country : citysearch.var}) ; console.log(citysearch.country) }}>Fetch</button>
+                </div>
+
+
+
+
+            {   
+                apidata.map((v, i) => v.Country == citysearch    ?
+
+                    <div className="nechy" key={i} >
 
                         <span>
-
+                            {v.Country}
                         </span>
 
 
 
                         <span>
                             Total Confirmed : {v.TotalConfirmed}
+                        </span>
+
+                        <span>
+                            Total Deaths : {v.TotalDeaths}
 
                         </span>
 
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span><p>  {apidata.Date}    </p></span>
 
-                        
+                        <span>
+                            New Deaths : {v.NewDeaths}
+                        </span>
+
+                        <span>
+                            Total Recovered : {v.TotalRecovered}
+                        </span>
+
+                        <span>
+                            {v.Date}
+                        </span>
 
 
 
-                        
 
-                        
+
+
+
+
 
                     </div>
 
                     :
 
                     null
-                        
-                )}
+
+                )
+            }
 
 
 
-            </div>
+
 
 
         </div>
